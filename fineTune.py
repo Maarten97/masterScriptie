1570255d27f3ba0641a1367e28e5d94151e330f4
Maarten97 <m.s.looijenga@student.utwente.nl>
2024-03-18 14:10:27 +0100

    Working reading text from XML file

--- fineTune.py
+++ fineTune.py
@@ -6,6 +6,8 @@ import xml.etree.ElementTree as ET
 ##Function to retrieve all XML files from Directory
 def load_data_from_xml(data_directory):
     data = []
+    emptyPrinter = 0
+    lineCounter = 0
     for filename in os.listdir(data_directory):
         print(filename)
         if filename.endswith(".xml"):
@@ -14,15 +16,21 @@ def load_data_from_xml(data_directory):
             tree = ET.parse(filepath)
             root = tree.getroot()
 
-            for item in root.findall("al"):
-                text = item.find("al").text.strip()
-                data.append(text)
-
+            for item in root.findall(".//al"):
+                al_text = item.text
+                if al_text is not None:
+                    data.append(al_text.strip())
+                    lineCounter += 1
+                else:
+                    print("al_text looks to be empty")
+                    emptyPrinter += 1
 
             print("Finished loading XML file: " + filename)
+    print(f"EmptyPrinter: {emptyPrinter}")
+    print(f"LineCounter: {lineCounter}")
     return data
 
 if __name__ == "__main__":
     dataDirectory = "C:/Users/looij/PycharmProjects/masterScriptie/Data/Wetboeken"
     dataSet = load_data_from_xml(dataDirectory)
-    print(dataSet)
+    # print(dataSet)
