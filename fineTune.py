c4a0f4af964c3212f15b4c36255310c599270c20
Maarten97 <m.s.looijenga@student.utwente.nl>
2024-03-18 15:39:19 +0100

    Working XML to CSV writer

--- fineTune.py
+++ fineTune.py
@@ -1,13 +1,23 @@
 ## This script will load and preprocess my dataset.
+import csv
 import os
 import xml.etree.ElementTree as ET
 
+csvWrite = True
+
 
 ##Function to retrieve all XML files from Directory
-def load_data_from_xml(data_directory):
+def load_data_from_xml(data_directory, csvFile):
     data = []
     emptyPrinter = 0
     lineCounter = 0
+
+    ##Create and Initialze CSV file
+    if csvWrite:
+        output_dir = os.path.dirname(csvFile)
+        os.makedirs(output_dir, exist_ok=True)
+
+    ##Read from XML and write to CSV
     for filename in os.listdir(data_directory):
         print(filename)
         if filename.endswith(".xml"):
@@ -16,14 +26,30 @@ def load_data_from_xml(data_directory):
             tree = ET.parse(filepath)
             root = tree.getroot()
 
-            for item in root.findall(".//al"):
-                al_text = item.text
-                if al_text is not None:
-                    data.append(al_text.strip())
-                    lineCounter += 1
-                else:
-                    print("al_text looks to be empty")
-                    emptyPrinter += 1
+            if csvWrite:
+                with open(csvFile, "a", newline="") as csvfile:
+                    fieldnames = ["bron", "text"]
+                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                    writer.writeheader()
+
+                    for item in root.findall(".//al"):
+                        al_text = item.text
+                        if al_text is not None:
+                                writer.writerow({"bron": os.path.splitext(filename)[0], "text": al_text.strip()})
+                                lineCounter += 1
+                        else:
+                            print("al_text looks to be empty")
+                            emptyPrinter += 1
+
+            else:
+                for item in root.findall(".//al"):
+                    al_text = item.text
+                    if al_text is not None:
+                            data.append(al_text.strip())
+                            lineCounter += 1
+                    else:
+                        print("al_text looks to be empty")
+                        emptyPrinter += 1
 
             print("Finished loading XML file: " + filename)
     print(f"EmptyPrinter: {emptyPrinter}")
@@ -32,5 +58,6 @@ def load_data_from_xml(data_directory):
 
 if __name__ == "__main__":
     dataDirectory = "C:/Users/looij/PycharmProjects/masterScriptie/Data/Wetboeken"
-    dataSet = load_data_from_xml(dataDirectory)
+    csvFile = "Data/Output/BurgelijkWetboekCSV.csv"
+    dataSet = load_data_from_xml(dataDirectory, csvFile)
     # print(dataSet)
