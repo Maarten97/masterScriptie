4ef152394e0762964afda7a185a58af23720562b
Maarten97 <m.s.looijenga@student.utwente.nl>
2024-05-15 13:37:50 +0200

    Package rename

---
+++ cleaning/statusCheck.py
@@ -0,0 +1,56 @@
+import os
+import csv
+import xml.etree.ElementTree as ET
+
+root_dir = 'D:/BWB/Subset2'
+output_dir = 'D:/BWB/Output/VersionControl'
+root_folder = ['202210_BWB_1', '202210_BWB_2', '202210_BWB_3', '202210_BWB_4']
+
+def manifest_lookup():
+    for folder in root_folder:
+        for root, dirs, files in os.walk(os.path.join(root_dir, folder)):
+            for filename in files:
+                if filename == 'manifest.xml':
+                    data = []
+                    manifest_path = os.path.join(root, filename)
+
+                    data.append(os.path.basename(os.path.dirname(manifest_path)))
+                    data.extend(manifest_reading(manifest_path))
+                    print(data)
+
+
+def manifest_reading(manifest_path):
+    data = []
+    tree = ET.parse(manifest_path)
+    root = tree.getroot()
+
+    primary_metadata = root.find("./metadata")
+    data.append(primary_metadata.find("datum_inwerkingtreding").text)
+    # status = ""
+    datum_intrekking = primary_metadata.find("datum_intrekking")
+    if datum_intrekking is not None:
+        datum_intrekking = datum_intrekking.text
+        status = "Vervallen"
+    else:
+        datum_intrekking = "N/A"
+        status = "Geldend"
+    data.append(status)
+    data.append(datum_intrekking)
+    return data
+
+
+def write_to_csv(data):
+    output_file = output_dir + "/statusCheck.csv"
+    os.makedirs(output_file, exist_ok=True)
+
+    with open(output_file, "a", newline="") as csvfile:
+        fieldnames = ["id", "Datum inwerking", "Status", "Datum intrekking"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+        if not write_header:
+            writer.writeheader()
+            write_header = True
+
+
+if __name__ == "__main__":
+    manifest_lookup()
+    print("Code statusCheck executed")
