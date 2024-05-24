8a1ef76a49a57f95bc2a3231bd554837c3ca7ae2
Maarten97 <m.s.looijenga@student.utwente.nl>
2024-05-24 11:26:07 +0200

    Code obsolete due to copying folder? future remove intended

--- cleaning/bwbVerdragSelect.py
+++ cleaning/bwbVerdragSelect.py
@@ -1,11 +1,14 @@
 import os
 import csv
+import xml.etree.ElementTree as ET
 
-root_dir = 'D:/BWB/Origineel/202210_BWB_4'
+# root_dir = 'D:/BWB/Origineel/202210_BWB_4'
+root_dir = 'C:/Users/looij/Documents/BWB/Subset2/202210_BWB_4'
 output_dir = 'D:/BWB/Output/Verdrag'
 error_file = 'D:/BWB/Output/Verdrag/error.txt'
 
 
+# Requires already that are not more versions in the file
 def folder_lookup():
     dirlist = []
     for bwbid in os.listdir(root_dir):
@@ -14,14 +17,18 @@ def folder_lookup():
                 dirlist.append(os.path.join(root_dir, bwbid))
     return dirlist
 
-def status_check(dirlist):
-    for direc in dirlist:
-        return None
 
-
+def language_check(dlist):
+    for direc in dlist:
+        for filename in os.listdir(direc):
+            if filename.endswith(".xml"):
+                print("Currently loading XML file: " + filename)
+                filepath = os.path.join(direc, filename)
+                tree = ET.parse(filepath)
+                root = tree.getroot()
 
 
 if __name__ == "__main__":
     dirlist = folder_lookup()
-    status_check(dirlist)
+    language_check(dirlist)
     print("Code statusCheck executed")
