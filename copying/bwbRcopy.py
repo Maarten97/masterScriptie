0e82ad02e9b1562667b4d0f311fdcbcac20fbc46
Maarten97 <m.s.looijenga@student.utwente.nl>
2024-05-27 13:10:24 +0200

    First version of BWBR finished

---
+++ copying/bwbRcopy.py
@@ -0,0 +1,38 @@
+import os
+import xml.etree.ElementTree as ET
+
+import bwbXMLprocess
+import bwbWriteGeneral
+
+root_dir = 'C:/Users/looij/Documents/BWB/Output'
+
+
+def folder_lookup():
+    dirlist = []
+    for bwbid in os.listdir(root_dir):
+        if bwbid.startswith('BWBR'):
+            if os.path.isdir(os.path.join(root_dir, bwbid)):
+                dirlist.append(os.path.join(root_dir, bwbid))
+    return dirlist
+
+
+def to_csv(dlist):
+    for direc in dlist:
+        for filename in os.listdir(direc):
+            if filename.endswith(".xml"):
+                print("Currently loading XML file: " + filename)
+                filepath = os.path.join(direc, filename)
+                bwbid = os.path.basename(direc)
+                tree = ET.parse(filepath)
+                root = tree.getroot()
+
+                for item in root.findall(".//al"):
+                    al_text = bwbXMLprocess.process_xml_text(item)
+                    if al_text is not None and al_text != "SKIP":
+                        bwbWriteGeneral.write_general(direc, bwbid, al_text)
+
+
+if __name__ == "__main__":
+    dirlist = folder_lookup()
+    to_csv(dirlist)
+    print("Code bwbWcopy executed")
