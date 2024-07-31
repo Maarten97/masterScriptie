7b63f983741c25344200d2a79fdec7c06bae1f9f
Maarten97 <m.s.looijenga@student.utwente.nl>
2024-07-31 16:30:27 +0200

    File can now handle processing of multiple csv files to merge into one txt

--- bertprep/csvToTxt.py
+++ bertprep/csvToTxt.py
@@ -1,8 +1,22 @@
 import csv
+import os
 import re
 
-input_dir = 'C:/Users/looijengam/Documents/DatasetFinal/random.csv'
-output_dir = 'output.txt'
+
+def merge_csv(inputdirs, tempdir):
+    csv.field_size_limit(10**9)
+    fieldnames = ["id", "text"]
+    with open(tempdir, mode='w', encoding='utf-8', newline='') as outfile:
+        writer = csv.DictWriter(outfile, fieldnames=fieldnames, quotechar='"', delimiter=';')
+        writer.writeheader()
+        for inputdir in inputdirs:
+            for csvfile in os.listdir(inputdir):
+                if csvfile.endswith('.csv'):
+                    newdir = os.path.join(input_dir, csvfile)
+                    with open(newdir, mode='r', encoding='utf-8') as csvinput:
+                        reader = csv.DictReader(csvinput, quotechar='"', delimiter=';')
+                        for row in reader:
+                            writer.writerow({'id': row['id'], 'text': row['text']})
 
 
 def split_into_sentences(text):
@@ -21,9 +35,21 @@ def process_csv_to_txt(input_dir, output_dir):
                     sentences = split_into_sentences(text)
                     for sentence in sentences:
                         txt_file.write(sentence + '\n')
-                    txt_file.write('\n\n')  # Add white lines between entries
+                    txt_file.write('\n')  # Add white lines between entries
+
+
+def main(inputs, outputs, tempdir):
+    if isinstance(inputs, list):
+        merge_csv(inputs, tempdir)
+        process_csv_to_txt(tempdir, outputs)
+    elif isinstance(inputs, str):
+        process_csv_to_txt(inputs, outputs)
+    else:
+        raise TypeError("Inputs should be a string or a list of strings.")
 
 
 # Example usage
 if __name__ == '__main__':
+    input_dir = 'C:/Users/looijengam/Documents/DatasetFinal/random.csv'
+    output_dir = 'output.txt'
     process_csv_to_txt(input_dir, output_dir)
