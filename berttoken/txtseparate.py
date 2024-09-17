3de111880d8c92bae129664fba7f500e24378405
Maarten97 <m.s.looijenga@student.utwente.nl>
2024-09-17 15:49:51 +0200

    Counting and splitting dataset

--- berttoken/txtseparate.py
+++ berttoken/txtseparate.py
@@ -1,9 +1,11 @@
+import csv
 import os
 
 # Global variables for filename and number of lines per file
 BASE_FILENAME = "output_file"
 LINES_PER_FILE = 2000000
 OUTPUT_FOLDER = "output"
+INPUT_FILE = "dataset.txt"
 
 
 def split_file(input_file):
@@ -45,6 +47,54 @@ def split_file(input_file):
             outfile.close()
 
 
+def count_lines(input_file, output_csv):
+    """
+    Parses the input file and writes the line number and word count to a CSV file.
+    """
+    line_counter = 0
+    with (open(input_file, 'r', encoding='utf-8', errors='ignore') as infile,
+          open(output_csv, 'w', newline='', encoding='utf-8') as csvfile):
+        csv_writer = csv.writer(csvfile)
+        # Write header for CSV
+        csv_writer.writerow(['Word Count'])
+
+        for line in infile:
+            line_counter += 1
+            # Count words by splitting the line based on whitespace
+            word_count = len(line.split())
+            # Write the line number and word count to the CSV
+            if word_count != 0:
+                csv_writer.writerow([word_count])
+
+        print(f'{line_counter} lines parsed')
+
+
+def find_lines(input_file, row_number):
+    line_counter = 0
+    with open(input_file, 'r', encoding='utf-8', errors='ignore') as infile:
+        for line in infile:
+            if line_counter == row_number:
+                print(f'Printing row: {row_number}')
+                print(line)
+                break
+            line_counter += 1
+
+
+def print_lines(input_file, output_txt):
+    line_counter = 0
+    with (open(input_file, 'r', encoding='utf-8', errors='ignore') as infile,
+          open(output_txt, 'w', newline='', encoding='utf-8') as txtfile):
+        for line in infile:
+            line_counter += 1
+            word_count = len(line.split())
+            if word_count > 500:
+                txtfile.write(f'At {line_counter} lines following sentence of {word_count} words:' + "\n")
+                txtfile.write(line + '\n')
+
+
 # Call the function with the path to your large txt file
 if __name__ == '__main__':
-    split_file("dataset.txt")
+    # count_lines(INPUT_FILE, "log2.csv")
+    # find_lines(INPUT_FILE, 1824223)
+    print_lines(INPUT_FILE, 'lines.txt')
+    # split_file(INPUT_FILE)
